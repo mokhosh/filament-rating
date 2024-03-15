b027f90abb252944b96d474da53b7daa5f170b6d
Mo Khosh <mskhoshnazar@gmail.com>
2024-03-16 02:16:28 +0330

    cleanup provider

--- src/FilamentRatingServiceProvider.php
+++ src/FilamentRatingServiceProvider.php
@@ -10,8 +10,6 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
-use Mokhosh\FilamentRating\Commands\FilamentRatingCommand;
-use Mokhosh\FilamentRating\Testing\TestsFilamentRating;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -24,18 +22,11 @@ class FilamentRatingServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package->name(static::$name)
             ->hasCommands($this->getCommands())
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
                     ->askToStarRepoOnGitHub('mokhosh/filament-rating');
             });
 
@@ -43,10 +34,6 @@ class FilamentRatingServiceProvider extends PackageServiceProvider
 
         if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
             $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
         }
 
         if (file_exists($package->basePath('/../resources/lang'))) {
@@ -86,9 +73,6 @@ class FilamentRatingServiceProvider extends PackageServiceProvider
                 ], 'filament-rating-stubs');
             }
         }
-
-        // Testing
-        Testable::mixin(new TestsFilamentRating());
     }
 
     protected function getAssetPackageName(): ?string
@@ -113,9 +97,7 @@ class FilamentRatingServiceProvider extends PackageServiceProvider
      */
     protected function getCommands(): array
     {
-        return [
-            FilamentRatingCommand::class,
-        ];
+        return [];
     }
 
     /**
