816058167982ac37faabd91d49dae642c7b46ca5
Mo Khosh <mskhoshnazar@gmail.com>
2024-03-16 04:55:13 +0330

    basic styling

--- resources/views/simple.blade.php
+++ resources/views/simple.blade.php
@@ -1,12 +1,18 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-    @foreach ($getStars() as $value)
-        <input
-            type="radio"
-            value="{{ $value }}"
-            id="star-{{ $value }}"
-            {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
-            wire:loading.attr="disabled"
-        />
-        <label for="star-{{ $value }}"></label>
-    @endforeach
+    <div class="flex group">
+        @foreach ($getStars() as $value)
+            <label for="star-{{ $value }}" class="text-primary-500 peer-checked:text-slate-300 group-hover:!text-primary-500 peer-hover:!text-slate-300 cursor-pointer">
+                <x-icon name="heroicon-s-star" class="w-6 h-6 pointer-events-none"/>
+            </label>
+
+            <input
+                type="radio"
+                value="{{ $value }}"
+                id="star-{{ $value }}"
+                class="hidden peer"
+                {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
+                wire:loading.attr="disabled"
+            />
+        @endforeach
+    </div>
 </x-dynamic-component>
