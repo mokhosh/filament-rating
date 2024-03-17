1cdeae43be59b7158e9f087fd51b764f3b494578
Mo Khosh <mskhoshnazar@gmail.com>
2024-03-17 03:31:05 +0330

    add rating column

---
+++ src/Columns/RatingColumn.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mokhosh\FilamentRating\Columns;
+
+use Filament\Tables\Columns\Column;
+use Mokhosh\FilamentRating\Concerns\HasStars;
+use Mokhosh\FilamentRating\RatingTheme;
+
+class RatingColumn extends Column
+{
+    use HasStars;
+
+    protected RatingTheme $theme = RatingTheme::Simple;
+
+    public function getView(): string
+    {
+        return $this->getTheme()->getView();
+    }
+
+    public function theme(RatingTheme $theme): static
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    public function getTheme(): RatingTheme
+    {
+        return $this->theme;
+    }
+}
