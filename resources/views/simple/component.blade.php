1a3b8b78e7f74e99d9e879e0254b5e169e4c2235
Marcel van Doornen <vandoornenmarcel@gmail.com>
2024-03-24 13:53:57 +0100

    Allow multiple rating fields in one form (#1)
    * Update the for and id field attributes with the form field name so you can use multiple start fields
    
    * remove unnecessary new method
    
    * use id to identify each field
    
    ---------
    
    Co-authored-by: Mo Khosh <mskhoshnazar@gmail.com>

--- resources/views/simple/component.blade.php
+++ resources/views/simple/component.blade.php
@@ -1,4 +1,5 @@
 @php
+    $id = $getId();
     $isDisabled = $isDisabled();
     $color = $getColor();
     $colorClass = match ($color) {
@@ -35,14 +36,14 @@
             <input
                 type="radio"
                 value="0"
-                id="star-0"
+                id="{{ $id }}-star-0"
                 class="!hidden peer"
                 @disabled($isDisabled)
                 {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
             >
 
             <label
-                for="star-0"
+                for="{{ $id }}-star-0"
                 @class([
                     "text-slate-300 peer-checked:text-danger-500",
                     "group-hover:!text-slate-300 peer-hover:!text-danger-500 cursor-pointer" => ! $isDisabled,
@@ -54,7 +55,7 @@
 
         @foreach ($getStarsArray() as $value)
             <label
-                for="star-{{ $value }}"
+                for="{{ $id }}-star-{{ $value }}"
                 @class([
                     "{$colorClass} peer-checked:text-slate-300",
                     "{$groupHoverColorClass} peer-hover:!text-slate-300 cursor-pointer" => ! $isDisabled,
@@ -66,7 +67,7 @@
             <input
                 type="radio"
                 value="{{ $value }}"
-                id="star-{{ $value }}"
+                id="{{ $id }}-star-{{ $value }}"
                 class="!hidden peer"
                 wire:loading.attr="disabled"
                 @disabled($isDisabled)
