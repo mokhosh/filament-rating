96666f64c2f35ab03506133d1bd1cff32ce22078
mokhosh <mokhosh@users.noreply.github.com>
2024-09-07 17:13:58 +0000

    Fix styling

--- src/FilamentRatingServiceProvider.php
+++ src/FilamentRatingServiceProvider.php
@@ -44,9 +44,7 @@ class FilamentRatingServiceProvider extends PackageServiceProvider
         }
     }
 
-    public function packageRegistered(): void
-    {
-    }
+    public function packageRegistered(): void {}
 
     public function packageBooted(): void
     {
